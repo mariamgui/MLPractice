93b55fbd0092b4c5d34955e018735f946562adcd
mariamgui <mariam.guizani@gmail.com>
2020-05-01 14:51:33 -0700

    added regression model train and test and accuracy measures

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,8 +1,11 @@
 import pandas as pd 
-import quandl
+import quandl, math
 from pprint import pprint
-import math
-
+import numpy as np
+from sklearn import preprocessing,  svm 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
 ## get stock data
 df = quandl.get('WIKI/GOOGL')
 ## Select only attribute needed
@@ -23,14 +26,29 @@ pprint(df.head())
 forecast_col = "Adj. Close"
 ## instead of deleting NA data
 df.fillna(-99999, inplace = True)
-## Using 10% of the data for prediction (using data from 10 days ago for prediction)
-forecast_out = int(math.ceil(0.01*len(df)))
-
+## Using 1% of the data for prediction (using data from 10 days ago for prediction)
+forecast_out = math.ceil(0.01*len(df))
 df ['label'] = df[forecast_col].shift(-forecast_out)
-#df.dropna(inplace=True)
-#pprint(df.tail())
 
-pprint(df.head())
+df.dropna(inplace=True)
+#pprint(df.tail())
+#pprint(df.head())
+
+X = np.array(df.drop(['label'],1))
+Y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+Y = np.array(df['label'])
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+clf = LinearRegression()
+clf.fit(X_train, Y_train)
+
+accuracy = clf.score(X_test, Y_test)
+
+print (accuracy)
+
+
 
 
  
