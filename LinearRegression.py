3c808801393c8316c2f360a7f442a2c950445699
mariamgui <mariam.guizani@gmail.com>
2020-05-01 12:49:37 -0700

    added forecast_col and forecast_out variable and a label column for prediction

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 import quandl
 from pprint import pprint
+import math
+
 ## get stock data
 df = quandl.get('WIKI/GOOGL')
 ## Select only attribute needed
@@ -18,6 +20,17 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 
 pprint(df.head())
 
+forecast_col = "Adj. Close"
+## instead of deleting NA data
+df.fillna(-99999, inplace = True)
+## Using 10% of the data for prediction (using data from 10 days ago for prediction)
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df ['label'] = df[forecast_col].shift(-forecast_out)
+#df.dropna(inplace=True)
+#pprint(df.tail())
+
+pprint(df.head())
 
 
  
